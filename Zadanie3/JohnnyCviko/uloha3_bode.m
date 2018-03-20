4628f78bb8c155e43fb6dd1d3ba1bfc1cb02c002
jan.sedivy <jan.sedivy@accenture.com>
2018-03-20 17:55:35 +0100

    Bode a Step pre 4 fun

--- Zadanie3/JohnnyCviko/uloha3_bode.m
+++ Zadanie3/JohnnyCviko/uloha3_bode.m
@@ -2,23 +2,23 @@
 Gs=tf([15000],[14 1 0]);
 forward=tf([15000],[14 -27 13],'ioDelay',1);
 figure
-step(forward)
-title('Step Response Forward');
+bode(forward)
+title('Bode Diagram Forward');
 %%%%tustin
 tustin=c2d(Gs,1,'tustin')
 tustin=tf([256 517.2 258.6],[1 -1.9311 0.931],'ioDelay',1);
 figure
-step(tustin)
-title('Step Response Tustin');
+bode(tustin)
+title('Bode Diagram Tustin');
 %%%backward
 backward=tf([15000 0 0],[15 29 14],'ioDelay',1);
 figure
-step(backward)
-title('Step Response Backward');
+bode(backward)
+title('Bode Diagram Backward');
 %nasa fun
 g=tf([15000],[14 1 0])
 gz = c2d(g, 1)
 gz = tf([523.2 510.9],[1 -1.931 0.9311],'ioDelay',1);
 figure 
-step(gz)
-title('Step Response Original')
+bode(gz)
+title('Bode Diagram Original')
