4d31d37692580c7e6b88b8fe7d6249e74cd38839
jan.sedivy <sedivy.jan.j@gmail.com>
2018-03-27 16:25:47 +0200

    Final version

--- Zadanie3/JohnnyCviko/uloha3.m
+++ Zadanie3/JohnnyCviko/uloha3.m
@@ -1,24 +1,29 @@
+g=tf([15000],[14 1 0])
 %%%forward
 Gs=tf([15000],[14 1 0]);
 forward=tf([15000],[14 -27 13]);
 figure
-step(forward)
-title('Step Response Forward');
+dstep([15000],[14 -27 13],20)
+hold on
+step(g,20)
+title('Step Response forward')
 %%%%tustin
 tustin=c2d(Gs,1,'tustin')
-tustin=tf([256 517.2 258.6],[1 -1.9311 0.931]);
+tustin=tf([258.6 517.2 258.6],[1 -1.9311 0.931]);
 figure
-step(tustin)
+dstep([15000 30000 15000],[58 -112 54],20)
+hold on
+step(g,20)
 title('Step Response Tustin');
 %%%backward
 backward=tf([15000 0 0],[15 29 14]);
 figure
-step(backward)
-title('Step Response Backward');
+dstep([15000 0 0],[15 -29 14],20)
 %nasa fun
 g=tf([15000],[14 1 0])
 gz = c2d(g, 1)
-gz = tf([523.2 510.9],[1 -1.931 0.9311],'ioDelay',1);
-figure 
-step(gz)
-title('Step Response Original')
+gz = tf([523.2 510.9],[1 -1.931 0.9311],1);
+hold on 
+step(g,20)
+title('Step Response Backward')
+tustin=c2d(Gs,1,'tustin')
