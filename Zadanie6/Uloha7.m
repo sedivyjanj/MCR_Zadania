bf6115c4a9d65e4f63ca951fb11c2609a27434bb
michal.rosival <michal.rosival@accenture.com>
2018-04-17 18:18:39 +0200

    zad6 ulo7

--- Zadanie6/Uloha7.m
+++ Zadanie6/Uloha7.m
@@ -3,11 +3,12 @@ gz=c2d(g,1)%perioda vzorkovania
 
 [B, A] = tfdata(gz, 'v')
 q0=1/sum(B)
-q1=A(2)*q0
-q2=A(3)*q0
+q0=q0*0.75
+q1=(A(2)-1)*q0+1/sum(B)
+q2=(A(3)-A(2))*q0+A(2)/sum(B)
 
 p2=B(2)*q0
-p3=B(3)*q0
+p3=(B(3)-B(2))*q0+B(2)/sum(B)
 Q=[q0 q1 q2]
 P=[1 -p2 -p2]
 P1=[1 0 -p2 -p3]
