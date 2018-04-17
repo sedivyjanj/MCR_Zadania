ab1e73d84414b66d2bc403be3b7e506114f8e13e
jan.sedivy <jan.sedivy@accenture.com>
2018-04-17 17:43:43 +0200

    commit

---
+++ Zadanie6/JohnnyCviko/uloha1.m
@@ -0,0 +1,16 @@
+%uloha1
+%ak by nevychadzalo zmenit d vo vzorci z prednasky na d=2
+b0=0;
+b1=0;
+b2=-510.87;
+b3=1544.9;
+a0=1;
+a1=-1.9311;
+a2=0.9311;
+
+q0=1/(b0+b1+b2+b3)
+q1=a1*q0
+q2=a2*q0
+
+p2=b2*q0
+p3=b3*q0
---
+++ Zadanie6/uloha5.m
@@ -0,0 +1,4 @@
+
+ret=tf([9.67*10^-4 -0.0019 9*10^-4],[1 0 0.4941 -1.4941],1,'Variable','z^-1')
+poly=pole(ret)
+absolut=abs(poly)
